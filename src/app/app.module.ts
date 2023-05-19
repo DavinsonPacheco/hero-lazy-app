4111e62cead9cc717c1e0a64465afbb8e047ca67
DavinsonPacheco <dfpachecoc@ufpso.edu.co>
2023-05-19 16:57:51 -0500

    variables de entorno + layout page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
