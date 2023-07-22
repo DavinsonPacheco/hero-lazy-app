ce6ce60eff0245e65ecb6d456d53376dcf3f668e
DavinsonPacheco <davinsonpacheco@wposs.com>
2023-07-22 07:42:50 -0500

    new page configuracion inicial

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,29 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
 
-    private baseUrl: string = environments.baseUrl;
+  private baseUrl: string = environments.baseUrl;
 
-    constructor(private http: HttpClient) { }
-    
-    getHeroes(): Observable<Hero[]>{
+  constructor(private http: HttpClient) { }
 
-        return this.http.get<Hero[]> (`${this.baseUrl}/heroes`)
-    }
+  getHeroes(): Observable<Hero[]> {
 
-    getHeroById( id: string): Observable < Hero | undefined >{
-        return this.http.get<Hero>(`${ this.baseUrl }/heroes/${id}`)
-            .pipe(
-                catchError( error => of( undefined ) )
-            );
-    }
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
 
-    getSuggestions( query: string ): Observable< Hero[] >{
-        return this.http.get< Hero[] >(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
-    }
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(error => of(undefined))
+      );
+  }
+
+  getSuggestions(query: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+
+    if (!hero.id) throw Error('The id hero is required!');
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(err => of(false)),
+        map(res => true)
+      )
+  }
+
+
 }
