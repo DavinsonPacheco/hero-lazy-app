8735dd004117086795a005ab8cd1d42cb0fcc08a
DavinsonPacheco <davinsonpacheco@wposs.com>
2023-08-07 21:44:59 -0500

    AUTH GUARD + PUBLIC GUARD

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class PublicGuard implements CanMatch, CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+    return this.authService.checkAuthentication()
+      .pipe(
+        tap( isAuthenticated => console.log(isAuthenticated)),
+        tap( isAuthenticated => {
+          if( isAuthenticated ){
+            this.router.navigate(['./']);
+          }
+        })
+      )
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.checkAuthStatus();
+  }
+  canMatch(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.checkAuthStatus();
+  }
+
+
+
+}
